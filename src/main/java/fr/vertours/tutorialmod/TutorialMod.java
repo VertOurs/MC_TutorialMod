7e83117758e4848bb94d78f7a5dd2ddd0bc0be04
VertOurs <vertours.dev@gmail.com>
2022-11-08 23:27:34 +0100

    2_Adding Items

--- src/main/java/fr/vertours/tutorialmod/TutorialMod.java
+++ src/main/java/fr/vertours/tutorialmod/TutorialMod.java
@@ -1,15 +1,17 @@
 package fr.vertours.tutorialmod;
 
+import fr.vertours.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
